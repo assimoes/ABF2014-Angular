b1ff355e5bd182d8281b8a7ab429466da3f7e2ab
assimoes <asvnsimoes@gmail.com>
2014-01-28 21:48:50 +0000

    Chopped the Christmas tree

--- abf2014-webapi-angular/ABFAPI/ABFAPI/app/importacao/importacao.js
+++ abf2014-webapi-angular/ABFAPI/ABFAPI/app/importacao/importacao.js
@@ -23,6 +23,7 @@
         vm.PhaseDesc = '';
         vm.IsProcessing = false;
 
+        var token = '';
         function activate() {
             common.activateController([], controllerId)
                .then(function () { log('Importacao de Clientes Activada'); });
@@ -32,32 +33,57 @@
             vm.IsProcessing = true;
             vm.billingResponse = [];
             vm.PhaseDesc = 'A obter credencial de facturação';
-            ambifacservice.GetAccessToken().then(function (t) {
-
-                var response = t[0].data;
-                var token = response.replace(/"/g, '');
+      
+            ambifacservice.GetAccessToken().then(_returnedToken, _error)
+          
+            function _returnedToken(data) {
+                var response = data[0].data;
+                token = response.replace(/"/g, '');
                 vm.PhaseDesc = 'A definir cliente(s) a faturar...Aguarde';
-                ambifacservice.ExecutePhase0(token).then(function (data) {
-                    vm.Phase = data[0].data[0].Phase;
-                    vm.PhaseDesc = 'A preencher tabelas auxiliares...Aguarde';
-                    ambifacservice.ExecutePhase1(token).then(function (data) {
-                        vm.Phase = data[0].data[0].Phase;
-                        vm.PhaseDesc = 'A aplicar regras de Faturação...Aguarde';
-                        ambifacservice.ExecutePhase2(token).then(function (data) {
-                            vm.Phase = data[0].data[0].Phase;
-                            vm.billingResponse = data[0].data;
-                            vm.PhaseDesc = 'A validar qualidade dos dados faturados...Aguarde';
-                            ambifacservice.ExecutePhase3(token).then(function (data) {
-                                vm.Phase = data[0].data[0].Phase;
-                            }).then(function (data) {
-                                vm.PhaseDesc = 'Processo concluido.';
-                                vm.Phase = '8';//fim
-                                vm.IsProcessing = false;
-                            })
-                        })
-                    })
-                })
-            });
+                ambifacservice.ExecutePhase0(token)
+                    .then(_executePhase0, _error);
+            }
+
+            function _executePhase0(response) {
+                vm.Phase = response[0].data[0].Phase;
+                vm.PhaseDesc = 'A preencher tabelas auxiliares...Aguarde';
+                ambifacservice.ExecutePhase1(token)
+                   .then(_executePhase1, _error);
+            }
+
+            function _executePhase1(response) {
+                vm.Phase = response[0].data[0].Phase;
+                vm.PhaseDesc = 'A aplicar regras de Faturação...Aguarde';
+                ambifacservice.ExecutePhase2(token)
+                   .then(_executePhase2, _error);
+            }
+
+
+            function _executePhase2(response) {
+                vm.Phase = response[0].data[0].Phase;
+                vm.billingResponse = response[0].data;
+                vm.PhaseDesc = 'A validar qualidade dos dados faturados...Aguarde';
+                ambifacservice.ExecutePhase3(token)
+                   .then(_executePhase3, _error);
+            }
+
+            function _executePhase3(response) {
+                vm.Phase = response[0].data[0].Phase;
+                ambifacservice.DataCleanUp(token)
+                    .then(_finishProcessing, _error);
+            }
+
+            function _finishProcessing(response){
+
+                token = '';
+                vm.PhaseDesc = 'Processo concluido.';
+                vm.Phase = '8';
+                vm.IsProcessing = false;
+            }
+
+            function _error(err) {
+                console.log(err.message);
+            }
         }
 
         activate();
