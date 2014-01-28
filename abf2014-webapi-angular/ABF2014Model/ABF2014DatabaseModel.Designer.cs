8aa67894c9cff55994c4276caad71120d214dab4
assimoes <asvnsimoes@gmail.com>
2014-01-28 21:49:40 +0000

    Pushed DataCleanUp stored procedure to the model.

--- abf2014-webapi-angular/ABF2014Model/ABF2014DatabaseModel.Designer.cs
+++ abf2014-webapi-angular/ABF2014Model/ABF2014DatabaseModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Desenvolvimentos\ABF 2014\ABF2014Model\ABF2014DatabaseModel.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\assimoes\Documents\GitHub\ABF2014-Angular\abf2014-webapi-angular\ABF2014Model\ABF2014DatabaseModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
